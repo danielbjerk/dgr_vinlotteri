745686f9f4b1b9a49c9efef859d068e3080c588d
Daniel Bjerkehagen <daniel.bjerkehagen@gmail.com>
2021-09-27 14:10:14 +0200

    Fix iteration over rows of df

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ fl_entry_cost = 5.0
 df_kontoutskrift = pd.read_from_excel(str_path)
 
 list_entries = []
-for row in df_kontoutskrift:
-    str_entree_name = df_kontoutskrift[str_name_col]
-    int_entree_ticket_amount = df_kontoutskrift[str_amount_col] // fl_entry_cost
+for _index, row in df_kontoutskrift.iterrows():
+    str_entree_name = row[str_name_col]
+    int_entree_ticket_amount = row[str_amount_col] // fl_entry_cost
     list_entries.append([str_entree_name] * int_entree_ticket_amount)
 
 random.choice(list_entries)
