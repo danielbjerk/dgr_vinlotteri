4df2e9f618d181ada3a8eec5277ef16feb5cd585
Daniel Bjerkehagen <danielbb@stud.ntnu.no>
2021-09-30 18:03:40 +0200

    Trekke to lodd istedenfor ett:)

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import numpy as np
 
 random.seed("DGR")
 
-str_path = "kontoutskrift.xlsx"    # Relative path
+str_path = "siste_transaksjoner.xlsx"    # Relative path
 str_name_col = "Forklaring"
 str_amount_col = "Inn på konto"
 fl_entry_cost = 5.0
 
-df_kontoutskrift = pd.read_from_excel(str_path)
+df_kontoutskrift = pd.read_excel(str_path)
 
 list_entries = []
+fl_payments = 0
 for _index, row in df_kontoutskrift.iterrows():
     str_entree_name = row[str_name_col]
-    int_entree_ticket_amount = row[str_amount_col] // fl_entry_cost
-    list_entries.append([str_entree_name] * int_entree_ticket_amount)
+    int_entree_ticket_amount = int(row[str_amount_col] // fl_entry_cost)
+    fl_payments += float(row[str_amount_col])
+    list_entries.extend([str_entree_name] * int_entree_ticket_amount)
 
-random.choice(list_entries)
+print(fl_payments)
+print(random.choice(list_entries))
+print(random.choice(list_entries))
